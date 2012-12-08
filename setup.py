3280750819426fbcf6cd468b60d7ac481d115052
David Danier <david.danier@team23.de>
2012-12-08 23:27:09 +0100

    Added setup.py and cleanup

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "django_callable_perms",
+    version = "0.1.0",
+    description = 'Modular registry of permission handlers for Django',
+    author = 'David Danier',
+    author_email = 'david.danier@team23.de',
+    url = 'https://github.com/ddanier/django_callable_perms',
+    long_description=open('README.rst', 'r').read(),
+    packages = [
+        'django_callable_perms',
+        'django_callable_perms.management',
+        'django_callable_perms.management.commands',
+        'django_callable_perms.templatetags',
+    ],
+    requires = [
+        'django(>=1.4)',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities'
+    ],
+)
+
