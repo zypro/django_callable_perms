4e73fe8df294585d44f8b7ae3b709daccccb4f50
David Danier <david.danier@team23.de>
2012-12-11 15:33:43 +0100

    Allow apps without '.' in name

--- django_callable_perms/utils.py
+++ django_callable_perms/utils.py
@@ -4,8 +4,6 @@ def autoload_permission_checks():
     from django.utils.module_loading import module_has_submodule
     
     for app in settings.INSTALLED_APPS:
-        if '.' not in app:
-            continue
         mod = import_module(app)
         try:
             import_module('%s.permissions' % app)
